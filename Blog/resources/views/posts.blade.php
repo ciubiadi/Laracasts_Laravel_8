ce787c67e74b717c3be6de483bbb3f86f376566b
Ciubucciu Adrian Albert <ciubucciu.adrianalbert@gmail.com>
2021-05-06 23:25:53 +0300

    Make a Route and Link to it

---
+++ Blog/resources/views/posts.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Ciubi's Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="./css/app.css" />
+        <script src="./js/app.js"></script>
+    </head>
+    <body class="antialiased">
+
+        <article>
+            <h1><a href="/post">My First Post</a></h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quo unde in exercitationem voluptatum impedit et veritatis sapiente temporibus! Placeat autem dolores minima corrupti beatae itaque dolore iste velit dolorum.</p>
+        </article>
+
+        <article>
+            <h1>My Second Post</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quo unde in exercitationem voluptatum impedit et veritatis sapiente temporibus! Placeat autem dolores minima corrupti beatae itaque dolore iste velit dolorum.</p>
+        </article>
+
+        <article>
+            <h1>My Third Post</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quo unde in exercitationem voluptatum impedit et veritatis sapiente temporibus! Placeat autem dolores minima corrupti beatae itaque dolore iste velit dolorum.</p>
+        </article>
+    </body>
+</html>
