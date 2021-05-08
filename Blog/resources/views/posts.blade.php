bfea0815c260b9af25a60b957d54d34e7ff4caa3
Ciubucciu Adrian Albert <ciubucciu.adrianalbert@gmail.com>
2021-05-09 00:50:33 +0300

    Managed to setup the env and database first migrations.

--- Blog/resources/views/posts.blade.php
+++ Blog/resources/views/posts.blade.php
@@ -14,7 +14,7 @@
 @section ('content')
     @foreach($posts as $post)
         <article>
-            <h1><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
+            <h1><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h1>
             <div> {{ $post->excerpt }} </div>
         </article>
     @endforeach
